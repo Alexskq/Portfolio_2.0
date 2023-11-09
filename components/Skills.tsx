f937a6fdb66b3cbe16982491057cc849f7a9d06e
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-11-09 14:26:01 +0100

    add logo and animation to skill section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,6 +3,16 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import SkillLeft from './SkillLeft'
 import SkillRight from './SkillRight'
+import SkillRails from './Skills/SkillRails'
+import SkillRuby from './Skills/SkillRuby'
+import SkillJavascript from './Skills/SkillJavascript'
+import SkillHtml from './Skills/SkillHtml'
+import SkillCss from './Skills/SkillCss'
+import SkillPostgresql from './Skills/SkillPostgresql'
+import SkillGit from './Skills/SkillGit'
+import SkillGithub from './Skills/SkillGithub'
+import SkillTailwind from './Skills/SkillTailwind'
+import SkillNextjs from './Skills/SkillNextjs'
 
 // type Props = {
 //   directionLeft: boolean;
@@ -22,17 +32,17 @@ function Skills() {
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over askill for currency profienciency</h3>
 
         <div className='grid grid-cols-4 gap-5'>
+            <SkillRails />
+            <SkillRuby />
+            <SkillJavascript />
+            <SkillHtml />
+            <SkillCss />
+            <SkillPostgresql />
+            <SkillGit />
+            <SkillGithub />
             <SkillLeft />
-            <SkillLeft />
-            <SkillLeft />
-            <SkillLeft />
-            <SkillRight />
-            <SkillRight />
-            <SkillRight />
-            <SkillRight />
-            <SkillLeft />
-            <SkillLeft/>
-            <SkillLeft/>
+            <SkillTailwind/>
+            <SkillNextjs />
         </div>
     </motion.div>
   )
--- components/Skills/SkillTailwind.tsx
+++ components/Skills/SkillTailwind.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 //     directionLeft: boolean;
 // }
 
-function SkillLeft() {
+export default function SkillRight() {
   return (
     <div className='group relative flex cursor-pointer'>
         <motion.img
@@ -15,7 +15,7 @@ function SkillLeft() {
         }}
         transition={{duration: 1}}
         whileInView={{opacity: 1, x: 0}}
-        src='https://www.sanity.io/static/images/logo_rounded_square.png'
+        src='tailwind.png'
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
 
@@ -27,5 +27,3 @@ function SkillLeft() {
     </div>
   )
 }
-
-export default SkillLeft
