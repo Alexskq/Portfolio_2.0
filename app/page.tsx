1f8d656ed0ec29587ee8a540b9b7bcdf956bfda7
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-11-22 22:21:47 +0100

    added back home button and get inspired

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,68 @@
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import WorkExperience from '@/components/WorkExperience'
-import Skills from  '@/components/Skills'
-import Projects from '@/components/Projects'
-import ContactMe from '@/components/ContactMe'
-import Link from 'next/link'
-import Image from 'next/image'
-import { Metadata } from 'next'
- 
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
+import Image from "next/image";
+import { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: 'Alex Portfolio',
-}
+  title: "Alex Portfolio",
+};
 
 export default function Home() {
-  
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-        {/* Header */}
-        <Header />
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      {/* Header */}
+      <Header />
 
-        {/* Hero  */}
+      {/* Hero  */}
 
-        <section id='hero' className='snap-start'>
-          <Hero />
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
 
-        </section>
+      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
-        {/* About */}
-        <section id="about" className='snap-center'>
-          <About />
-        </section>
+      {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
-        {/* Experience */}
-        <section id="experience" className='snap-center'>
-          <WorkExperience />
-        </section>
+      {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
-        {/* Skills */}
-        <section id='skills' className='snap-start'>
-          <Skills />
+      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
-        </section>
+      {/* Contact me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
 
-        {/* Projects */}
-        <section id='projects' className='snap-start'>
-          <Projects /> 
-        </section>
-
-        {/* Contact me */}
-        <section id='contact' className='snap-start'>
-          <ContactMe />
-        </section>
-
-        {/* <Link href="#hero">
-          <footer className='sticky bottom-5 w-full cursor-pointer'>
-            <div className='flex items-center justify-center'>
-              <Image
-                alt="footer"
-                src="/profil picture.jpg"
-                width={400}
-                height={400}
-                className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0'
-              />
-            </div>
-          </footer>
-        </Link> */}
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image
+              alt="footer"
+              src="/profil picture.jpg"
+              width={400}
+              height={400}
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0"
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
-  )
+  );
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,24 +1,25 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { motion } from 'framer-motion'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 function Projects({}: Props) {
-    const projects = [1, 2, 3, 4]
+  const projects = [1, 2, 3, 4];
   return (
     <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{opacity: 1}}
-        transition={{duration: 1.5}}
-        className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-            Projects
-        </h3>
-        
-        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-            {/* {projects.map((project, i ) => (
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+        {/* {projects.map((project, i ) => (
                 <div key="" className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                    <motion.img
                     initial={{
@@ -58,122 +59,186 @@ function Projects({}: Props) {
                 
                 </div>
             ))} */}
-            {/* Projects */}
-            <div key="1" className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-                   <motion.img
-                    initial={{
-                        y:-300,
-                        opacity: 0
-                    }}
-                    transition={{duration: 1.2}}
-                    whileInView={{opacity: 1, y: 0}}
-                    viewport={{once: true }}
-                    src="/portfolio.png"
-                    alt='portfolio'
-                    width={300}
-                    height={300}
-                    className='rounded-lg'
-                   />
-                   <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-                        <h4 className='text-4xl font-semibold text-center underline decoration-[#F7AB0A]/50'>
-                            Mon Portfolio
-                        </h4>
-                    
-                        <div className='flex items-center space-x-2 justify-center'>
-                            <Image
-                            className='h-10 w-10'
-                            src="/Nextjs.png"
-                            alt='next'
-                            width={40}
-                            height={40}
-                         />
-                            <Image
-                            className=' h-10 w-10'
-                            src="/tailwind.png"
-                            alt='tailwind'
-                            width={50}
-                            height={40}
-                            />
-                            <Image
-                            className=' h-10 w-10 rounded-full'
-                            src='/sanity.png'
-                            alt='sanity'
-                            width={50}
-                            height={40}
-                            />
-                        </div>
+        {/* Projects */}
+        <div
+          key="1"
+          className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+        >
+          <motion.img
+            initial={{
+              y: -300,
+              opacity: 0,
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            src="/portfolio.png"
+            alt="portfolio"
+            width={300}
+            height={300}
+            className="rounded-lg"
+          />
+          <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+            <h4 className="text-4xl font-semibold text-center underline decoration-[#F7AB0A]/50">
+              Mon Portfolio
+            </h4>
 
-                        <p className='text-lg text-center md:text-left'>
-                        🎯 Après avoir suivi le tuto et la doc&apos; j&apos;ai décidé de me lancer dans ce projet et de faire ce portfolio en Next.JS et TailwindCss
-                        🚧 Des améliorations sont en cours pour implémenter le CMS Sanity, afin d&apos;améliorer le code et faciliter la mise à jour du projet... Wait & See 🔜
-                        </p>
-                    </div>
-                
-                </div>
-            {/* Projects */}
-            <div key="" className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-                   <motion.img
-                    initial={{
-                        y:-300,
-                        opacity: 0
-                    }}
-                        transition={{duration: 1.2}}
-                        whileInView={{opacity: 1, y: 0}}
-                        viewport={{once: true }}
-                        src="/Panima.png"
-                        alt="Merci Mathilde pour l'image"
-                        width={300}
-                        height={300}
-                   />
-                   <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-                        <h4 className='text-4xl font-semibold text-center underline decoration-[#F7AB0A]/50'>
-                            Panima
-                        </h4>
-                    <div className='flex items-center space-x-2 justify-center'>
-                        <Image
-                            className=' h-10 w-10'
-                            src="/Ruby on Rails Logo.png"
-                            alt='tech use'
-                            width={40}
-                            height={30}
-                        />
-                          <Image
-                            className=' h-10 w-10'
-                            src="/pngwing.com.png"
-                            alt='tech use'
-                            width={40}
-                            height={30}
-                        />
-                        <Image
-                            className=' h-10 w-10'
-                            src="/Javascript.png"
-                            alt='tech use'
-                            width={40}
-                            height={30}
-                        />
-                        <Image
-                            className=' h-10 w-10'
-                            src="/PostgreSQL Logo.png"
-                            alt='tech use'
-                            width={40}
-                            height={30}
-                        />
-                    </div>
+            <div className="flex items-center space-x-2 justify-center">
+              <Image
+                className="h-10 w-10"
+                src="/Nextjs.png"
+                alt="next"
+                width={40}
+                height={40}
+              />
+              <Image
+                className=" h-10 w-10"
+                src="/tailwind.png"
+                alt="tailwind"
+                width={50}
+                height={40}
+              />
+              <Image
+                className=" h-10 w-10 rounded-full"
+                src="/sanity.png"
+                alt="sanity"
+                width={50}
+                height={40}
+              />
+            </div>
 
-                    <div className='text-lg px-20 text-center md:text-left '>
-                        <span>📊 Panima est une plateforme en version MVP qui permet de gérer un magasin autonome, project de fin de formation crée en collaboration lors du bootcamp Le Wagon à Lille</span>
-                        <span>🛒 Un magasin autonome est un magasin sans caisse ni caissier. Grâce à cette interface, le responsable du magasin peut suivre à distance et en temps réel.</span>
-                    </div>
-                   </div>
-                
-                </div>
-            {/* Projects */}
+            <p className="text-lg text-center md:text-left">
+              🎯 Après avoir suivi le tuto et la doc&apos; j&apos;ai décidé de
+              me lancer dans ce projet et de faire ce portfolio en Next.JS et
+              TailwindCss 🚧 Des améliorations sont en cours pour implémenter le
+              CMS Sanity, afin d&apos;améliorer le code et faciliter la mise à
+              jour du projet... Wait & See 🔜
+            </p>
+          </div>
+        </div>
+        {/* Projects */}
+        <div
+          key=""
+          className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+        >
+          <motion.img
+            initial={{
+              y: -300,
+              opacity: 0,
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            src="/getinspired.png"
+            alt="Merci Mathilde pour l'image"
+            width={300}
+            height={300}
+          />
+          <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+            <h4 className="text-4xl font-semibold text-center underline decoration-[#F7AB0A]/50">
+              Get inspired...
+            </h4>
+            <div className="flex items-center space-x-2 justify-center">
+              <Image
+                className=" h-10 w-10"
+                src="/Nextjs.png"
+                alt="tech use"
+                width={40}
+                height={30}
+              />
+              <Image
+                className=" h-10 w-10"
+                src="/tailwind.png"
+                alt="tech use"
+                width={40}
+                height={30}
+              />
+            </div>
+
+            <div className="text-lg px-20 text-center md:text-left ">
+              <span>
+                📊 A la base ce n&apos;était qu&apos;un exercice où il fallait
+                crée la logique pour générer un message aléatoire, que j&apos;
+                décidé de transformer en petit projet Next.js
+              </span>
+            </div>
+          </div>
         </div>
 
-        <div className=' w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12 '>
+        {/* Project */}
+        <div
+          key=""
+          className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+        >
+          <motion.img
+            initial={{
+              y: -300,
+              opacity: 0,
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            src="/Panima.png"
+            alt="Merci Mathilde pour l'image"
+            width={300}
+            height={300}
+          />
+          <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+            <h4 className="text-4xl font-semibold text-center underline decoration-[#F7AB0A]/50">
+              Panima
+            </h4>
+            <div className="flex items-center space-x-2 justify-center">
+              <Image
+                className=" h-10 w-10"
+                src="/Ruby on Rails Logo.png"
+                alt="tech use"
+                width={40}
+                height={30}
+              />
+              <Image
+                className=" h-10 w-10"
+                src="/pngwing.com.png"
+                alt="tech use"
+                width={40}
+                height={30}
+              />
+              <Image
+                className=" h-10 w-10"
+                src="/Javascript.png"
+                alt="tech use"
+                width={40}
+                height={30}
+              />
+              <Image
+                className=" h-10 w-10"
+                src="/PostgreSQL Logo.png"
+                alt="tech use"
+                width={40}
+                height={30}
+              />
+            </div>
+
+            <div className="text-lg px-20 text-center md:text-left ">
+              <span>
+                📊 Panima est une plateforme en version MVP qui permet de gérer
+                un magasin autonome, project de fin de formation crée en
+                collaboration lors du bootcamp Le Wagon à Lille
+              </span>
+              <span>
+                🛒 Un magasin autonome est un magasin sans caisse ni caissier.
+                Grâce à cette interface, le responsable du magasin peut suivre à
+                distance et en temps réel.
+              </span>
+            </div>
+          </div>
         </div>
+        {/* Projects */}
+      </div>
+      {/* Project */}
+
+      <div className=" w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12 "></div>
     </motion.div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
